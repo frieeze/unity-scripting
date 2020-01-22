917c7b50f9a44dcda5028fa6793c847f99f1837a
Surbayrole Alexis <frieeze@users.noreply.github.com>
2020-01-22 23:44:41 +0100

    ellipse rotation

--- Assets/Scripts/Controller/EllipseController.cs
+++ Assets/Scripts/Controller/EllipseController.cs
@@ -6,22 +6,26 @@ public class EllipseController : MonoBehaviour
 {
    private Transform shapeHolder;
    [SerializeField] private GameObject prefab = default;
-   [SerializeField] private Vector3 rotationCenter = default;
    [SerializeField] private float width = 2f;
    [SerializeField] private float height = 5f;
    [SerializeField] private int numberOfPoints = 6;
    [SerializeField] private Vector3 rotationSpeed = new Vector3(1,1,1);
+   [SerializeField] private Vector3 ellipseRotationSpeed = default;
 
    private Vector3 rotationAngle = default;
+   private Vector3 ellipseRotationAngle = default;
 
    private void Awake()
    {
       shapeHolder = transform;
    }
    // Start is called before the first frame update
-   void Start()
+    void Start()
    {
       StartCoroutine(UpdateShapes());
+   }
+
+    void Update() {
    }
 
    private IEnumerator UpdateShapes()
@@ -43,12 +47,23 @@ public class EllipseController : MonoBehaviour
          // Instantiate new spheres
          foreach (Vector3 circlePoint in circlePoints)
             Instantiate(prefab, circlePoint, target, shapeHolder);
+         
+         foreach (Transform child in shapeHolder){
+            child.transform.RotateAround(shapeHolder.position, Vector3.right, ellipseRotationAngle.x);
+            child.transform.RotateAround(shapeHolder.position, Vector3.up, ellipseRotationAngle.y);
+            child.transform.RotateAround(shapeHolder.position, Vector3.forward, ellipseRotationAngle.z);
+         }
 
          rotationAngle = new Vector3( 
             (rotationAngle.x + rotationSpeed.x*0.1f)%360,
             (rotationAngle.y + rotationSpeed.y*0.1f)%360,
             (rotationAngle.z + rotationSpeed.z*0.1f)%360 
             );
+         ellipseRotationAngle = new Vector3(
+            (ellipseRotationAngle.x + ellipseRotationSpeed.x *0.1f)%360,
+            (ellipseRotationAngle.y + ellipseRotationSpeed.y *0.1f)%360,
+            (ellipseRotationAngle.z + ellipseRotationSpeed.z *0.1f)%360
+         );
 
       }
    }
@@ -61,9 +76,9 @@ public class EllipseController : MonoBehaviour
       {
          float rad = 2 * Mathf.PI * i / numberOfPoints;
          circlePoints.Add(new Vector3(
-             rotationCenter.x + width * Mathf.Cos(rad),
-             rotationCenter.y + height * Mathf.Sin(rad),
-             rotationCenter.z));
+             shapeHolder.position.x + width * Mathf.Cos(rad) ,
+             shapeHolder.position.y + height * Mathf.Sin(rad),
+             shapeHolder.position.z));
       }
 
       return circlePoints;
